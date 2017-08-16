ee9d5edc259770d5825f64e4f0c3ebce845cfef1
mymac <liuzn@yunheit.com>
2017-08-16 12:23:44 +0800

    add login

--- app/containers/login/LoginForm.js
+++ app/containers/login/LoginForm.js
@@ -1,98 +1,67 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {hashHistory} from 'react-router'
+import { hashHistory } from 'react-router'
 // import UserSessionActions from '../actions/UserSessionActions'
 
 import '../../static/css/login.css'
 
 class LoginForm extends React.Component {
-  constructor(props, context) {
-    super(props, context)
-    this.state = {
-      username: "",
-      password: "",
-      email: "",
-      LoginTab: true,
-      permanent: true
-    }
-  }
-  
-  handleShowLoginTab() {
-    this.setState({LoginTab: true})
-  }
-  handleShowSignupTab() {
-    this.setState({LoginTab: false})
-  }
-  handleChange = e => {
-    var newState = {}
-    newState[e.target.name] = e.target.name === "permanent" ? e.target.checked : e.target.value
-    this.setState(newState)
-  }
-  handleLogin = e => {
-    e.preventDefault()
-    var data = {
-      username : this.state.username,
-      password : this.state.password,
-      permanent : this.state.permanent
-    }
-    hashHistory.push('/home')
-    // UserSessionActions.login(data)
-  }
-  handleSignup(e) {
-    e.preventDefault()
-    // UserSessionActions.signup(this.state.email)
-  }
-  render() {
-    return (
-      <div className="login-form">
-        <div className="login-tab clearfix">
-          <a className={this.state.LoginTab ? "active": ""} onClick={this.handleShowLoginTab}>Log In</a>
-          <a className={this.state.LoginTab ? "": "active"} onClick={this.handleShowSignupTab}>Sign Up</a>
-        </div>
-
-        <form method="post" onSubmit={ this.handleLogin } style={this.state.LoginTab ? {} : { display: "none"}}>
-          <div className="form-field">
-            <input type="text" placeholder="Username/Email" aria-label="Username or Email" name="username" required value={ this.state.username } onChange={ this.handleChange }></input>
-          </div>
-          <div className="form-field">
-            <input type="password" placeholder="Password" aria-label="Password" name="password" required value={ this.state.password } onChange={ this.handleChange }></input>
-          </div>
-          <label className="form-check">
-            <input type="checkbox" name="permanent" checked={ this.state.permanent } onChange={ this.handleChange }></input> Remember Me
-          </label>
-          <div className="form-submit">
-            <button className="button buttong--green">Log In</button>
-            <a> Find Password</a>
-          </div>
-        </form>
-
-        <form method="post" onSubmit={ this.handleSignup } style={this.state.LoginTab ? { display: "none"} : {}}>
-        <div className="form-field">
-          <input type="email" placeholder="Email" aria-label="Email" name="email" required value={ this.state.email } onChange={ this.handleChange }></input>
-        </div>
-        <div className="form-submit">
-          <button className="button buttong--green">Sign Up</button>
-        </div>
-        </form>
-        {
-          ((obj) => {
-            if ( obj.state.LoginTab ) {
-              return (
-                <div className="login-social">
-                  <h3>Login With</h3>
-                  <div className="login-buttons">
-                    <a className="button login">
-                      <i className="qc-icon-google"></i> GOOGLE
-                    </a>
-                  </div>
-                </div>
-              )
-            }
-          })(this)
+    constructor(props, context) {
+        super(props, context)
+        this.state = {
+            title: '数据运营中心',
+            username: "",
+            password: "",
+            hint: "",
         }
-      </div>
-    )
-  }
+    }
+    static contextTypes = { router: React.PropTypes.object };
+    handleChange = e => {
+        var newState = {};
+        newState[e.target.name] = e.target.value;
+        console.log(newState);
+        this.setState(newState);
+    }
+    isSubmit = () =>{
+        let postData = {
+            username : this.state.username,
+            password : this.state.password,
+        }
+        if(postData.username.length<1){
+            this.setState({ hint: '用户姓名不能为空'});
+            setTimeout(() => {
+                this.setState({ hint: ''});
+            }, 3000);
+            return
+        }
+        if(postData.password.length<1){
+            this.setState({ hint: '密码不能为空'});
+            setTimeout(() => {
+                this.setState({ hint: ''});
+            }, 3000);
+            return
+        }
+        console.log('可以去登录页了');
+        this.context.router.push('/home');  
+    }
+    handleSignup(e) {
+        e.preventDefault()
+        // UserSessionActions.signup(this.state.email)
+    }
+    render() {
+        return (
+            <div className="loginContainer  -align-center">
+                <div className="loginContent">
+                    <div className="loginTitle">{this.state.title}</div>
+                    <div className="loginText">登&nbsp;录</div>
+                    <input value={ this.state.username } onChange={ this.handleChange }  name="username" className="loginaccount" type="text"></input>
+                    <input value={ this.state.password } onChange={ this.handleChange } name="password" className="loginPassword" type="text"></input>
+                    <div   className="loginBtn" onClick={this.isSubmit}>登录</div>
+                    <div className="testHint">{this.state.hint}</div>
+                </div>
+            </div>
+        )
+    }
 }
 
 module.exports = LoginForm
